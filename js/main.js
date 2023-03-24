0b84a3db42b67a666a3f21aa14eb21550596f305
Sevdat <sevdat1999@gmail.com>
2023-03-24 16:42:44 +0100

    until 4.15

---
+++ js/main.js
@@ -0,0 +1,47 @@
+//2.17
+//Функция, возвращающая случайное целое число из переданного диапазона включительно.
+function randomInt(from, to){
+  if (isNaN(from) || isNaN(to)) { throw new Error('Error'); };
+  var from = Math.abs(from);
+  var to = Math.abs(to);
+  let random = Math.random();
+return parseInt((from < to) ? from + (random*(to-from)) : to + (random*(from-to)));
+};
+
+//Функция для проверки максимальной длины строки. Будет использоваться для проверки
+//длины введённого комментария, но должна быть универсальна.
+//eslint-disable-next-line
+function lengthOfString(str,size){
+  return (str.length == size) ? true : false
+}
+
+//4.15
+class Information {
+  constructor(Id, url, description, likes, comments){
+    this.Id = Id;
+    this.url = url;
+    this.description = description;
+    this.likes = likes;
+    this.comments = comments;
+  }
+};
+function randomString(from, to){
+ let str = ""
+  for (let index = 0; index < randomInt(from,to); index++) {
+   str = str + String.fromCharCode(randomInt(65,90));
+  }
+  return str
+};
+function randomGenerator(){
+  let list = [];
+  for (let index = 0; index < 25; index++) {
+    let listSize = list.length;
+    let id = (listSize != 0) ? list[listSize - 1].Id + 1 : 0;
+    let url = `photos/${id}.jpg`
+    let description = `${id}.${randomString(0,100)}` ;
+    let likes = randomInt(15,200);
+    let comments = randomString(0,200)
+    list.push(new Information(id,url,description,likes,comments));
+  };
+  return list;
+}
