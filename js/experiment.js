994c7e02019cca1b0d357e9f2e393a41fdf731ec
Sevdat <sevdat1999@gmail.com>
2023-04-07 23:22:27 +0200

    all pictures and live server extension

--- js/experiment.js
+++ js/experiment.js
@@ -1,14 +1,14 @@
-import {Information} from "./xdata.js";
+//eslint-disable-next-line
 function print(value){
   return console.log(value);
 }
 
 //2.17
 //Функция, возвращающая случайное целое число из переданного диапазона включительно.
-function getRandomInt(from, to){
+function randomInt(from, to){
   if (isNaN(from) || isNaN(to)) { throw new Error('Error') };
-  from = Math.abs(from);
-  to = Math.abs(to);
+  var from = Math.abs(from);
+  var to = Math.abs(to);
   const random = Math.random();
 return parseInt((from < to) ? from + (random*(to-from)) : to + (random*(from-to)));
 }
@@ -16,31 +16,38 @@ return parseInt((from < to) ? from + (random*(to-from)) : to + (random*(from-to)
 //Функция для проверки максимальной длины строки. Будет использоваться для проверки
 //длины введённого комментария, но должна быть универсальна.
 //eslint-disable-next-line
-function checkLengthOfString(str,size){
-  return (str.length === size) ? true : false;
+function lengthOfString(str,size){
+  return (str.length == size) ? true : false;
 }
 
 //4.15
-function getRandomString(from, to){
+class Information {
+  constructor(Id, url, description, likes, comments){
+    this.Id = Id;
+    this.url = url;
+    this.description = description;
+    this.likes = likes;
+    this.comments = comments;
+  }
+}
+function randomString(from, to){
  let str = '';
- let random = getRandomInt(from,to)
-  for (let index = 0; index < random; index++) {
-   str = str + String.fromCharCode(getRandomInt(65,90));
+  for (let index = 0; index < randomInt(from,to); index++) {
+   str = str + String.fromCharCode(randomInt(65,90));
   }
   return str;
 }
-
-export function getRandomInformationList(amountProduced, descriptionFrom, descroptionTo, likesFrom, likesTo, commentsFrom,commentsTo){
+function randomGenerator(amountProduced, descriptionFrom, descroptionTo, likesFrom, likesTo, commentsFrom,commentsTo){
   const list = [];
   for (let index = 0; index < amountProduced; index++) {
     const listSize = list.length;
     const id = (listSize !== 0) ? list[listSize - 1].Id + 1 : 0;
     const url = `photos/${id}.jpg`;
-    const description = `${id}.${getRandomString(descriptionFrom,descroptionTo)}`;
-    const likes = getRandomInt(likesFrom,likesTo);
-    const comments = getRandomInt(commentsFrom,commentsTo);
+    const description = `${id}.${randomString(descriptionFrom,descroptionTo)}`;
+    const likes = randomInt(likesFrom,likesTo);
+    const comments = randomString(commentsFrom,commentsTo);
     list.push(new Information(id,url,description,likes,comments));
   }
   return list;
 }
-
+//print(randomGenerator(25, 0, 100, 15, 200, 0, 200));
