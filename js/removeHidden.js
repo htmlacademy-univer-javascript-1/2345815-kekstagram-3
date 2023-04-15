fd64ccf6a774ca2da0486195bba821b5c8ef8371
Sevdat <sevdat1999@gmail.com>
2023-04-15 18:53:36 +0200

    fixes3

--- js/removeHidden.js
+++ js/removeHidden.js
@@ -2,18 +2,18 @@ const removeHidden = document.querySelector('.img-upload__overlay');
 const locateHidden = document.querySelector('#upload-file');
 const closeButton = document.querySelector('#upload-cancel');
 const resetData = document.querySelector('.img-upload__form');
-function close(e){
-  if (closeButton.id === e.target.id || e.key === 'Escape' ) {
-  removeHidden.classList.add('hidden');resetData.reset();
-  document.body.classList.remove('model-open');
+function close(e) {
+  if (closeButton.id === e.target.id || e.key === 'Escape') {
+    removeHidden.classList.add('hidden'); resetData.reset();
+    document.body.classList.remove('model-open');
+  }
 }
-}
-function open(){
+function open() {
   removeHidden.classList.remove('hidden');
   document.body.classList.add('model-open');
 }
 locateHidden.addEventListener('change', open);
-closeButton.addEventListener('click', close );
+closeButton.addEventListener('click', close);
 document.addEventListener('keyup', close);
 
 //window.onclick = e => {if (c.id == e.target.id) {f.classList.add('hidden'); console.log(e)} }
--- js/scale.js
+++ js/scale.js
@@ -3,17 +3,17 @@ const meter = document.querySelector('.scale__control--value');
 const smaller = document.querySelector('.scale__control--smaller');
 const bigger = document.querySelector('.scale__control--bigger');
 const imageSize = document.querySelector('.img-upload__preview');
-function forSmaller(){
-  const k = meter.value.replace('%','');
+function forSmaller() {
+  const k = meter.value.replace('%', '');
   const check = Number(k) - 25;
   meter.value = (check > 25) ? `${check}%` : '25%';
-  imageSize.style.transform = `scale(${meter.value.replace('%','')/100})`;
+  imageSize.style.transform = `scale(${meter.value.replace('%', '') / 100})`;
 }
-function forBigger(){
-  const k = meter.value.replace('%','');
+function forBigger() {
+  const k = meter.value.replace('%', '');
   const check = Number(k) + 25;
-  meter.value = (check < 100) ? `${check}%` : "100%";
-  imageSize.style.transform = `scale(${meter.value.replace('%','')/100})`;
+  meter.value = (check < 100) ? `${check}%` : '100%';
+  imageSize.style.transform = `scale(${meter.value.replace('%', '') / 100})`;
 }
-smaller.addEventListener('click', forSmaller );
-bigger.addEventListener('click', forBigger );
+smaller.addEventListener('click', forSmaller);
+bigger.addEventListener('click', forBigger);
