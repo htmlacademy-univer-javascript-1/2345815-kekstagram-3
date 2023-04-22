30aa293fdb41116b150468d830b29dae52a853e9
Sevdat <sevdat1999@gmail.com>
2023-04-22 04:01:57 +0200

    hopefully last

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 import { getRandomInformationList } from './util.js';
-import { pastePhotos } from './pastePhotos.js';
+import { pastePhotos, serverJson } from './pastePhotos.js';
 import { } from './removeHidden.js';
 import { } from './scale.js';
 import { } from './effects.js';
 
 const informationArray = getRandomInformationList(25, 0, 100, 15, 200, 0, 200);
 pastePhotos(informationArray);
-
+serverJson('https://27.javascript.pages.academy/kekstagram-simple/data')
